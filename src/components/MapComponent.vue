cc00612bea5710789cf92b0e2f9e9e89846bdcee
Felix <felix.p.magnus@gmail.com>
2017-08-15 17:08:42 +0200

    added basic gmaps integration

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -5,12 +5,17 @@
         <div class="buttons">
             <div class="row justify-content-center">
                 <button class="btn btn-primary temp-btn-margin" v-on:click="handlePlaceButtonClick('cairns')">Cairns</button>
-            </div>
-            <div class="row justify-content-center">
                 <button class="btn btn-primary temp-btn-margin" v-on:click="handlePlaceButtonClick('brisbane')">Brisbane</button>
+                <button class="btn btn-primary temp-btn-margin" v-on:click="handlePlaceButtonClick('sydney')">Sydney</button>
             </div>
             <div class="row justify-content-center">
-                <button class="btn btn-primary temp-btn-margin" v-on:click="handlePlaceButtonClick('sydney')">Sydney</button>
+                <div class="col align-middle">
+                    <gmap-map class="gmap"
+                        :center="{lat:-33.865143, lng:151.209900}"
+                        :zoom="7"
+                        map-type-id="terrain"
+                    ></gmap-map>
+                </div>
             </div>
         </div>
     </div>
@@ -39,6 +44,10 @@
 
 <style scoped>
     .temp-btn-margin {
-        margin-top: 10px;
+        margin: 10px;
+    }
+
+    .gmap {
+        height: calc(100vh - 165px);
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'animate.css'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = true
+
+Vue.use(VueGoogleMaps, {
+    load: {
+        key: 'AIzaSyCYDsC1hsS1WuBBWceVWhba3OuatvZ79qI',
+        libraries: 'places' // This is required if you use the Autocomplete plugin
+        // OR: libraries: 'places,drawing'
+        // OR: libraries: 'places,drawing,visualization'
+        // (as you require)
+    }
+})
 
 /* eslint-disable no-new */
 new Vue({
